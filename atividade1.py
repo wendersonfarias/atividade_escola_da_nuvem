01778fe7e898688bc52a7587755486217d718314
Wenderson Farias <65517254+wendersonfarias@users.noreply.github.com>
2025-07-23 00:25:54 +0000

    atividade feitas

--- atividade1.py
+++ atividade1.py
@@ -1,5 +1,5 @@
 # 1- Programa de Saudação
 # * Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
 
-print("OLÁ MUNDO!"
+print("OLÁ MUNDO!")
 print("EU ESTOU APRENDENDO PYTHON!");
---
+++ atividade4.py
@@ -0,0 +1,20 @@
+#4- Calculadora de Preço Total
+#* Desenvolva um programa que calcula o preço total de uma compra. 
+#Use as seguintes informações:
+
+#* Nome do produto: "Cadeira Infantil"
+#* Preço unitário: R$ 12.40
+#* Quantidade: 3
+#O programa deve calcular o preço total e exibir todas as informações, 
+# incluindo o resultado final.
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3  
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
